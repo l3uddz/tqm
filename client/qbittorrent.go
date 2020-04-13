9992133b6b2831bb78dc7a22c86b50ddca2c4cc0
l3uddz <l3uddz@gmail.com>
2020-04-13 12:08:07 +0100

    change(qbittorrent): api version error message change

--- client/qbittorrent.go
+++ client/qbittorrent.go
@@ -81,7 +81,7 @@ func (c *QBittorrent) Connect() error {
 	// retrieve & validate api version
 	apiVersion, err := c.client.WebAPIVersion()
 	if err != nil {
-		return errors.WithMessage(err, "failed determining api version")
+		return errors.WithMessage(err, "failed retrieving api version")
 	} else if apiVersion < 2.2 {
 		return fmt.Errorf("unsupported webapi version: %v", apiVersion)
 	}
