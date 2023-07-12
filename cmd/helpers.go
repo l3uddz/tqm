33692ece783584a1a9bd6ad6c83b2f8b503d1d9c
KaiserBh <kaiserbh@proton.me>
2023-07-13 01:07:09 +1000

    refactor: refactored as the function was getting too big.

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -81,30 +81,25 @@ type HashTorrentPair struct {
 	Torrent config.Torrent
 }
 
-// remove torrents that meet remove filters
-func removeEligibleTorrents(log *logrus.Entry, c client.Interface, torrents map[string]config.Torrent,
-	tfm *torrentfilemap.TorrentFileMap) error {
-	// vars
-	ignoredTorrents := 0
-	softRemoveTorrents := 0
-	hardRemoveTorrents := 0
-	errorRemoveTorrents := 0
-	var removedTorrentBytes int64 = 0
-	torrentRetentionLimit := config.Config.TorrentRetentionLimit
-
-	// Create a slice from the map: (Total torrent)
+func getHashTorrentPairs(torrents map[string]config.Torrent) []HashTorrentPair {
 	hashTorrentPairs := make([]HashTorrentPair, 0, len(torrents))
 	for h, t := range torrents {
 		hashTorrentPairs = append(hashTorrentPairs, HashTorrentPair{Hash: h, Torrent: t})
 	}
+	return hashTorrentPairs
+}
 
-	// Sort the slice by AddedSeconds:
-	sort.Slice(hashTorrentPairs, func(i, j int) bool {
-		return hashTorrentPairs[i].Torrent.AddedSeconds < hashTorrentPairs[j].Torrent.AddedSeconds
+func sortTorrentsByAddedSeconds(torrents []HashTorrentPair) {
+	sort.Slice(torrents, func(i, j int) bool {
+		return torrents[i].Torrent.AddedSeconds < torrents[j].Torrent.AddedSeconds
 	})
+}
 
-	//Filter out all torrents that can be removed
+func filterTorrents(hashTorrentPairs []HashTorrentPair, log *logrus.Entry, c client.Interface, torrents map[string]config.Torrent) ([]HashTorrentPair, int, int) {
 	removeTorrents := make([]HashTorrentPair, 0)
+	ignoredTorrents := 0
+	errorRemoveTorrents := 0
+
 	for _, pair := range hashTorrentPairs {
 		h := pair.Hash
 		t := pair.Torrent
@@ -133,100 +128,18 @@ func removeEligibleTorrents(log *logrus.Entry, c client.Interface, torrents map[
 		}
 	}
 
-	// Sort the removeTorrents slice by AddedSeconds to make sure oldest torrents are removed first
-	sort.Slice(removeTorrents, func(i, j int) bool {
-		return removeTorrents[i].Torrent.AddedSeconds < removeTorrents[j].Torrent.AddedSeconds
-	})
+	return removeTorrents, ignoredTorrents, errorRemoveTorrents
+}
 
-	NonRemovableTorrent := len(hashTorrentPairs) - len(removeTorrents)
-	removeCount := torrentRetentionLimit - NonRemovableTorrent
-
-	if removeCount < 0 {
-		for _, pair := range removeTorrents {
-			h := pair.Hash
-			t := pair.Torrent
-
-			// torrent meets the remove filters
-			// are the files unique and eligible for a hard deletion (remove data)
-			uniqueTorrent := tfm.IsUnique(t)
-			removeMode := "Soft"
-
-			if uniqueTorrent {
-				// this torrent does not contains files found within other torrents (remove its data)
-				removeMode = "Hard"
-			}
-
-			// remove the torrent
-			log.Info("-----")
-			if !t.FreeSpaceSet {
-				log.Infof("%s removing: %q - %s", removeMode, t.Name, humanize.IBytes(uint64(t.DownloadedBytes)))
-			} else {
-				// show current free-space as well
-				log.Infof("%s removing: %q - %s - %.2f GB", removeMode, t.Name,
-					humanize.IBytes(uint64(t.DownloadedBytes)), t.FreeSpaceGB())
-			}
-
-			log.Infof("Ratio: %.3f / Seed days: %.3f / Seeds: %d / Label: %s / Tracker: %s / "+
-				"Tracker Status: %q", t.Ratio, t.SeedingDays, t.Seeds, t.Label, t.TrackerName, t.TrackerStatus)
-
-			if !flagDryRun {
-				// do remove
-				removed, err := c.RemoveTorrent(t.Hash, uniqueTorrent)
-				if err != nil {
-					log.WithError(err).Fatalf("Failed removing torrent: %+v", t)
-					// dont remove from torrents file map, but prevent further operations on this torrent
-					delete(torrents, h)
-					errorRemoveTorrents++
-					continue
-				} else if !removed {
-					log.Error("Failed removing torrent...")
-					// dont remove from torrents file map, but prevent further operations on this torrent
-					delete(torrents, h)
-					errorRemoveTorrents++
-					continue
-				} else {
-					log.Info("Removed")
-
-					// increase free space (if its a hard remove)
-					if uniqueTorrent && t.FreeSpaceSet {
-						log.Tracef("Increasing free space by: %s", humanize.IBytes(uint64(t.DownloadedBytes)))
-						c.AddFreeSpace(t.DownloadedBytes)
-						log.Tracef("New free space: %.2f GB", c.GetFreeSpace())
-					}
-
-					time.Sleep(1 * time.Second)
-				}
-			} else {
-				log.Warn("Dry-run enabled, skipping remove...")
-			}
-
-			if uniqueTorrent {
-				// increased hard removed counters
-				removedTorrentBytes += t.DownloadedBytes
-				hardRemoveTorrents++
-			} else {
-				// increase soft remove counters
-				softRemoveTorrents++
-			}
-
-			// remove the torrent from the torrent file map
-			tfm.Remove(t)
-			delete(torrents, h)
-		}
-
-		// show result
-		log.Info("-----")
-		log.Infof("Ignored torrents: %d", ignoredTorrents)
-		log.WithField("reclaimed_space", humanize.IBytes(uint64(removedTorrentBytes))).
-			Infof("Removed torrents: %d hard, %d soft and %d failures",
-				hardRemoveTorrents, softRemoveTorrents, errorRemoveTorrents)
-		return nil
-	}
-
-	removeCount = len(removeTorrents) - removeCount
+func processRemoveTorrents(removeCount int, removeTorrents []HashTorrentPair, log *logrus.Entry, c client.Interface, tfm *torrentfilemap.TorrentFileMap, torrents map[string]config.Torrent) (int64, int, int, int) {
+	softRemoveTorrents := 0
+	hardRemoveTorrents := 0
+	errorRemoveTorrents := 0
+	var removedTorrentBytes int64 = 0
 
 	for i := 0; i < removeCount; i++ {
 		pair := removeTorrents[i]
+
 		h := pair.Hash
 		t := pair.Torrent
 
@@ -297,6 +210,35 @@ func removeEligibleTorrents(log *logrus.Entry, c client.Interface, torrents map[
 		tfm.Remove(t)
 		delete(torrents, h)
 	}
+
+	return removedTorrentBytes, hardRemoveTorrents, softRemoveTorrents, errorRemoveTorrents
+}
+
+func removeEligibleTorrents(log *logrus.Entry, c client.Interface, torrents map[string]config.Torrent,
+	tfm *torrentfilemap.TorrentFileMap) error {
+
+	torrentRetentionLimit := config.Config.TorrentRetentionLimit
+
+	hashTorrentPairs := getHashTorrentPairs(torrents)
+	sortTorrentsByAddedSeconds(hashTorrentPairs)
+
+	//Filter out all torrents that can be removed
+	removeTorrents, ignoredTorrents, errorRemoveTorrents := filterTorrents(hashTorrentPairs, log, c, torrents)
+
+	// Sort the removeTorrents slice by AddedSeconds to make sure oldest torrents are removed first
+	sortTorrentsByAddedSeconds(removeTorrents)
+
+	NonRemovableTorrent := len(hashTorrentPairs) - len(removeTorrents)
+	removeCount := torrentRetentionLimit - NonRemovableTorrent
+
+	if removeCount < 0 {
+		removeCount = len(removeTorrents)
+	} else {
+		removeCount = len(removeTorrents) - removeCount
+	}
+
+	removedTorrentBytes, hardRemoveTorrents, softRemoveTorrents, errorRemoveTorrents :=
+		processRemoveTorrents(removeCount, removeTorrents, log, c, tfm, torrents)
 
 	// show result
 	log.Info("-----")
